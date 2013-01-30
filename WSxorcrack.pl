485779a65786f8e780c49ab8ace1c5f172912785
Eldar Marcussen <wireghoul@justanotherhacker.com>
2013-01-30 00:13:04 -0500

    WebSphere XOR cracker

---
+++ WSxorcrack.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+# Breaks Websphere Passwords stored in the {XOR} format
+# Usage: WSxorcracker.pl "{XOR}abc="
+#
+use strict;
+use warnings;
+use MIME::Base64;
+
+
+my $rawstr = $ARGV[0];
+$rawstr=~ s/^{XOR}//;
+print "$rawstr\n";
+my $xorstr = decode_base64($rawstr);
+for my $echar (split //, $xorstr) {
+  my $pchar = $echar ^ "_";
+  print $pchar;
+}
+print "\n";
