49436108c15fb24cc4e84c9c9294d407b586a6ac
Eldar Marcussen <wireghoul@justanotherhacker.com>
2016-09-19 04:07:27 -0400

    VM changed IP address

--- myproxy.sh
+++ myproxy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+#socat tcp-listen:3306,bind=192.168.224.128,fork,reuseaddr tcp-connect:192.168.224.128:4040 &
 touch /var/log/mysql/proxy.query.log
 gnome-terminal --title "Mysql query log" -e 'tail -f /var/log/mysql/proxy.query.log' &
 echo "Mysql proxy starting on port 4040"
